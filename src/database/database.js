17a4a90f5a58c455e4a728bac73091240384c831
hacarapi <huberacarapi@gmail.com>
2024-12-01 02:18:54 -0400

    secure ssl

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 import 'dotenv/config'
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
+    process.env.DB_DATABASE,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
